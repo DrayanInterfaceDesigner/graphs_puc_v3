a961d8635dc4bd1241f6841d1d3648e56ed9c2b5
Drayan Silva Magalhães <31392046+DrayanInterfaceDesigner@users.noreply.github.com>
2024-04-10 04:00:00 -0300

    1mont

--- main.py
+++ main.py
@@ -26,6 +26,34 @@ graph.add_edge('B', 'C', 5)
 
 graph.remove_vertice('D')
 
-print(graph.get_adjacent('A'))
+print(graph.get_weight('A', 'B'))
+graph.set_weight('A', 'B', 50)
+graph.set_weight('C', 'A', 25)
+print(graph.get_weight('A', 'B'))
+
+print(
+    f'A out degree: {graph.out_degree("A")}',
+    f'B out degree: {graph.out_degree("B")}',
+    f'C out degree: {graph.out_degree("C")}',
+)
+
+print(
+    f'A in degree: {graph.in_degree("A")}',
+    f'B in degree: {graph.in_degree("B")}',
+    f'C in degree: {graph.in_degree("C")}',
+)
+
+print(
+    f'A degree: {graph.degree("A")}',
+    f'B degree: {graph.degree("B")}',
+    f'C degree: {graph.degree("C")}',
+)
+
+
+graph.to_string_list()
+
+print(graph.get_adjacencies('A'))
+print(graph.get_adjacencies('B'))
+print(graph.get_adjacencies('C'))
 
 print(graph)
