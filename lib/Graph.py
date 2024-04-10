3949c9bf41f3097abf7edc9fbc3bb6ad72bc12d9
acculloko <lucca.lucchin@gmail.com>
2024-04-10 03:44:55 -0300

    commit gamer

--- lib/Graph.py
+++ lib/Graph.py
@@ -1,4 +1,5 @@
-
+import time
+# import numpy as np
 
 class Graph:
     def __init__(self, directed:bool, weighted:bool, representation:str) -> None:
@@ -20,15 +21,21 @@ class Graph:
             if ((connection['parent'] == vertice_a and connection['child'] == vertice_b) or 
                 (connection['parent'] == vertice_b and connection['child'] == vertice_a)):
                 return True
-            else: 
-                return False
+        return False
                 
     def add_vertice(self, name:str) -> None:
         if not self.find_vertice(name):
             self.vertices.append({len(self.vertices): name})
     
+<<<<<<< Updated upstream
     def add_edge(self, parent:str, child:str, weight:int=1) -> None:
         self.connections.append({'parent': parent, 'child': child, 'weight': weight})
+=======
+    def add_edge(self, parent:str, child:str, weight:int|float=1) -> None:
+        w:int|float = 1 if not self.weighted else weight
+        self.connections.append({'parent': parent, 'child': child, 'weight': w})
+        self.connections.append({'parent': child, 'child': parent, 'weight': w})
+>>>>>>> Stashed changes
     
     def remove_vertice(self, name:str ) -> None:
         to_remove:list = []
@@ -84,9 +91,112 @@ class Graph:
             return self.in_degree(vertice) + self.out_degree(vertice)
         else:
             return self.in_degree(vertice)
+
+
+    def depth_search(self, start:str, end:str) -> int:
+        if not self.find_vertice(start) or not self.find_vertice(end):
+            return 0
+        
+        start_time = time.perf_counter()
+
+        stack = []
+        visited = []
+        stack.append(start)
+
+        while len(stack) > 0:
+            vertice = stack.pop(-1)
+            if vertice not in visited:
+                visited.append(vertice)
+            for adjacency in sorted(self.get_adjacencies(vertice)):
+                if adjacency not in visited:
+                    stack.append(adjacency)
+            if vertice == end:
+                break
+
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+
+        return visited, total_time
+
+    def width_search(self, start:str, end:str) -> int:
+        if not self.find_vertice(start) or not self.find_vertice(end):
+            return 0
+        
+        start_time = time.perf_counter()
+
+        queue = []
+        visited = []
+        queue.append(start)
+
+        while len(queue) > 0:
+            vertice = queue.pop(0)
+            if vertice not in visited:
+                visited.append(vertice)
+            for adjacency in sorted(self.get_adjacencies(vertice)):
+                if adjacency not in visited:
+                    queue.append(adjacency)
+            if vertice == end:
+                break
+
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+
+        return visited, total_time
     
+<<<<<<< Updated upstream
+=======
+    # dijkstra helper, determines min cost vertice
+    def extract_min(self, q, costs):
+        min_cost_vertice = None
+        min_weight = +1e10
+        for vertice in q:
+            if costs[vertice] <= min_weight:
+                min_weight = costs[vertice]
+                min_cost_vertice = vertice
+        return min_cost_vertice
+
+>>>>>>> Stashed changes
     def dijkstra(self, start:str, end:str) -> int:
-        pass
+        if not self.find_vertice(start) or not self.find_vertice(end):
+            return 0
+        
+        start_time = time.perf_counter()
+
+        pi = {}
+        costs = {}
+        q = []
+        path = []
+        total_cost = 1e10
+        named_vertices = [list(x.values())[0] for x in self.vertices]
+
+        for vertice in named_vertices:
+            costs[vertice] = +1e10 # infinite
+            pi[vertice] = None
+        costs[start] = 0.0
+        for vertice in named_vertices:
+            q.append(vertice)
+        while len(q) > 0:
+            vertice = self.extract_min(q, costs)
+            if vertice is None:
+                break
+            q.remove(vertice)
+            for adjacency in self.get_adjacencies(vertice):
+                new_cost = costs[vertice] + self.get_weight(vertice, adjacency)
+                if new_cost < costs[adjacency]:
+                    costs[adjacency] = new_cost
+                    pi[adjacency] = vertice
+        if pi[end] != None:
+            total_cost = costs[end]
+            vertice = end
+            while vertice != None:
+                path.insert(0, vertice)
+                vertice = pi[vertice]
+
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+
+        return path, total_cost, total_time
+
 
     def warshall(self) -> list:
         pass
--- main.py
+++ main.py
@@ -21,29 +21,76 @@ graph.add_vertice('C')
 graph.add_edge('A', 'B', 5)
 graph.add_edge('B', 'A', 10)
 graph.add_edge('B', 'C', 5)
+graph.add_vertice('D')
+graph.add_vertice('E')
+graph.add_edge('A', 'B', 2)
+graph.add_edge('A', 'C', 4)
+graph.add_edge('B', 'C', 6)
+graph.add_edge('D', 'C', 1)
+graph.add_edge('D', 'E', 2)
+graph.add_edge('C', 'E', 3)
 # graph.remove_vertice('A')
 # graph.remove_edge('A', 'B')
 
 graph.remove_vertice('D')
+# graph.remove_vertice('D')
 
+<<<<<<< Updated upstream
 print(
     f'A out degree: {graph.out_degree("A")}',
     f'B out degree: {graph.out_degree("B")}',
     f'C out degree: {graph.out_degree("C")}',
 )
+=======
+# print(graph.get_weight('A', 'B'))
+# graph.set_weight('A', 'B', 50)
+# graph.set_weight('C', 'A', 25)
+# print(graph.get_weight('A', 'B'))
 
 print(
     f'A in degree: {graph.in_degree("A")}',
     f'B in degree: {graph.in_degree("B")}',
     f'C in degree: {graph.in_degree("C")}',
 )
+# print(
+#     f'A out degree: {graph.out_degree("A")}',
+#     f'B out degree: {graph.out_degree("B")}',
+#     f'C out degree: {graph.out_degree("C")}',
+# )
+>>>>>>> Stashed changes
 
 print(
     f'A degree: {graph.degree("A")}',
     f'B degree: {graph.degree("B")}',
     f'C degree: {graph.degree("C")}',
 )
+# print(
+#     f'A in degree: {graph.in_degree("A")}',
+#     f'B in degree: {graph.in_degree("B")}',
+#     f'C in degree: {graph.in_degree("C")}',
+# )
 
+# print(
+#     f'A degree: {graph.degree("A")}',
+#     f'B degree: {graph.degree("B")}',
+#     f'C degree: {graph.degree("C")}',
+# )
+
+<<<<<<< Updated upstream
 print(graph.get_adjacent('A'))
+=======
+print(graph.get_adjacencies('C'))
+>>>>>>> Stashed changes
 
 print(graph)
+
+print("depth search A -> E")
+print(graph.depth_search('A', 'E'))
+
+print("width search A -> E")
+print(graph.width_search('A', 'E'))
+
+print(graph.vertices)
+
+print(graph)path, cost, time = graph.dijkstra('A', 'E')
+print(f"Shortest path between A and E: {path} with cost: {cost}. found in time: {time}")
